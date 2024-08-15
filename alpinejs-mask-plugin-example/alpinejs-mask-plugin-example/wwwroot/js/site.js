7299b392708e85b41829d0de43a79fdad6622dad
Fabricio Oliveira <152199383+ciandt-fabriciooliveira@users.noreply.github.com>
2024-08-15 18:12:50 -0300

    add input mask examples

---
+++ alpinejs-mask-plugin-example/alpinejs-mask-plugin-example/wwwroot/js/site.js
@@ -0,0 +1,8 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+function addHttps(input) {
+    if (!input.startsWith('https://')) {
+        return 'https://' + input;
+    }
+    return input;
+}
